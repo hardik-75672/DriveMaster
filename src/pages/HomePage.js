52b8585d3ca6185507772e7f4dede227d058c5a8
hardik <hardik96500c@gmail.com>
2024-04-22 16:35:32 +0530

    update HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,25 +1,44 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import SideNavigate from "../components/SideNavigate";
 import Content from "../components/Content";
 import Header from "../components/Header";
 import { selectUser } from "../redux/Slice/authSlice";
 import { useDispatch, useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { getdataAsync } from "../redux/Slice/folderSlice";
+import CreateFolder from "../components/dashboard/CreateFolder";
+import HomeComponent from "../components/dashboard/homeComponent/HomeComponent";
+import { Home } from "@mui/icons-material";
+import FolderComponent from "../components/dashboard/folder/FolderComponent";
 const HomePage = () => {
+  const [isCreateFolderModelOpen, setIsCreateFolderModelOpen] = useState(false);
   const user = useSelector(selectUser);
   const dispatch = useDispatch();
   console.log(user);
   dispatch(getdataAsync());
-  useEffect(() => {});
+  console.log(isCreateFolderModelOpen);
   return (
     <>
       {/* {!user && <Navigate to="/login"></Navigate>} */}
       <div>
         <Header />
         <div className="flex">
-          <SideNavigate />
-          <Content />
+          {isCreateFolderModelOpen && (
+            <CreateFolder
+              setIsCreateFolderModelOpen={setIsCreateFolderModelOpen}
+            />
+          )}
+          <SideNavigate
+            setIsCreateFolderModelOpen={setIsCreateFolderModelOpen}
+          />
+          <Routes>
+            <Route path="" element={<HomeComponent></HomeComponent>}></Route>
+            <Route
+              path="/folder/:folderId"
+              element={<FolderComponent></FolderComponent>}
+            ></Route>
+          </Routes>
+          {/* <HomeComponent /> */}
         </div>
       </div>
     </>
