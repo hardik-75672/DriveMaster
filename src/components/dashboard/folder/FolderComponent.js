d87105f4305a9fc228048783605b7106c5bdee7a
hardik <hardik96500c@gmail.com>
2024-04-28 10:53:36 +0530

    subFolder UI

--- src/components/dashboard/folder/FolderComponent.js
+++ src/components/dashboard/folder/FolderComponent.js
@@ -1,15 +1,36 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import BreadCrumb from "../homeComponent/BreadCrumb";
+import { shallowEqual, useSelector } from "react-redux";
+import Content from "../../Content.js";
+import {
+  selectUserFolder,
+  selectisLoading,
+} from "../../../redux/Slice/folderSlice";
+// import Content from "../../Content";
 
 const FolderComponent = ({ folder }) => {
   const { folderId } = useParams();
-  console.log("me aa gya");
+  const allFolder = useSelector(selectUserFolder);
+  const isLoading = useSelector(selectisLoading);
+
+  const myFolder = allFolder.filter((folder) => folder.userId === folderId);
+  // const p_length = myFolder[0].parent.length;
+  const childFolder = allFolder.filter(
+    (folder) => folder.parent[folder.parent.length - 1] === folderId
+  );
+
+  console.log(childFolder);
   return (
-    <div>
-      <BreadCrumb folder={folder} />
-      this is your is {folderId}
-    </div>
+    <>
+      <div className="block">
+        <BreadCrumb />
+        {childFolder.map(() => {
+          <div>hello</div>;
+        })}
+        {!isLoading && <Content type={"folder"} userFolder1={childFolder} />}
+      </div>
+    </>
   );
 };
 
