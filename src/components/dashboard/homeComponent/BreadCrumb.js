1bf27875e86deb45cb3f5c20162a2eb3992b56ab
hardik <hardik96500c@gmail.com>
2024-04-28 10:52:20 +0530

    BreadCrumb UI

--- src/components/dashboard/homeComponent/BreadCrumb.js
+++ src/components/dashboard/homeComponent/BreadCrumb.js
@@ -4,16 +4,16 @@ import { selectCurrentFolder } from "../../../redux/Slice/folderSlice";
 
 const BreadCrumb = ({ folder }) => {
   const currentFolder = useSelector(selectCurrentFolder);
-  console.log(folder);
+  console.log(currentFolder);
 
   return (
     <div>
       <nav
         aria-label="breadcrumb"
-        className="w-full p-4 bg-gray-100 text-gray-800"
+        className="w-full p-4 bg-white-50 text-gray-100"
       >
         <ol className="flex h-8 space-x-2 text-gray-800">
-          {folder.map((item) => {
+          {currentFolder.map((item) => {
             return (
               <li className="flex items-center">
                 <a
@@ -22,11 +22,21 @@ const BreadCrumb = ({ folder }) => {
                   title="Back to homepage"
                   className="flex items-center hover:underline"
                 >
-                  {item}
+                  {item} /
                 </a>
               </li>
             );
           })}
+          {/* <li className="flex items-center">
+            <a
+              rel="noopener noreferrer"
+              href="#"
+              title="Back to homepage"
+              className="flex items-center hover:underline"
+            >
+              item
+            </a>
+          </li> */}
         </ol>
       </nav>
     </div>
